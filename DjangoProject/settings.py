680a82afa6003a9190f033d0e1fc70bc057bc595
piotr_wojciechowski <piowo1@o2.pl>
2025-02-26 17:53:48 +0100

    Next changes

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -12,9 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+ALLOWED_HOSTS = ['DjangoProject.onrender.com']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
